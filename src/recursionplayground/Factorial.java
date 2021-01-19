e1fde1278fc361a17b86b759094ed52a5bbcb1a9
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-01-20 00:43:46 +0200

    factorial that takes long integers

--- src/recursionplayground/Factorial.java
+++ src/recursionplayground/Factorial.java
@@ -13,8 +13,30 @@ import java.util.Scanner;
  */
 public class Factorial {
 
-    public static void main(String[] args) {
-
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
+        long fact = 0;
+
+        System.out.println("");
+        System.out.println(
+                "The program wil take any number, ranging from 0 to 256 and calculate its factorial value.");
+        System.out.println(
+                "Please provide a  number that fits the range parameters (0 to 256), the factorial value of which you wish to calculate: ");
+        System.out.println("");
+        long number = sc.nextInt();
+        
+        fact = factorial(number);
+        System.out.println("The factorial of " + number + " is " + fact);
+
     }
+
+    private static long factorial(long n) {
+        if ((n == 0) || (n == 1)) {
+            return 1;
+        } else {
+            return (n * factorial(n - 1));
+        }
+    }
+
 }
