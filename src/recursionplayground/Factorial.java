cd7da83581527e3a9e944a8cf199d9cf9fa7afe7
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-01-19 22:56:38 +0200

    initial commit

---
+++ src/recursionplayground/Factorial.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recursionplayground;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jokubas
+ */
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+    }
+}
