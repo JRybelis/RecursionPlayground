34907c30af615954b9074a07450b2bf0eadf5c20
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-01-21 13:58:12 +0200

    Fibonacci sequence printer. Still requires a max value break clause.

--- src/recursionplayground/Fibonacci.java
+++ src/recursionplayground/Fibonacci.java
@@ -5,6 +5,8 @@
  */
 package recursionplayground;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jokubas
@@ -15,7 +17,51 @@ public class Fibonacci {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("");
+        System.out.println(
+                "The program will take any number, ranging from 1 to 50, as a member integer of the Fibonacci sequence. It will then calculate and display the values of Fibonacci sequence members leading to it.");
+        System.out.println("");
+
+        long userInput = 0;
+        long index = 0;
+        boolean breakClause = true;
+        Scanner persistScanner = new Scanner(System.in);
+
+        while ((userInput <= 0) || (userInput > 1000)) {
+            System.out.println(
+                    "Please provide a number that fits the range parameters of 1 to 50, for the position in the Fibonacci sequence.");
+            String inputStringified = persistScanner.nextLine();
+
+            try {
+                userInput = Integer.valueOf(inputStringified);
+            } catch (NumberFormatException e) {
+                userInput = 0;
+            }
+       }
+        while (breakClause = true) {
+            if ( index == userInput) {
+                breakClause = false;
+            }
+            System.out.println("The number " + index
+                + " in the Fibonacci sequence is: " + fibonacci(index) + ".");
+            index++;
+        }
+        System.out.println("The end of the sequence.");
+
     }
-    
+
+    public static long fibonacci(long index) {
+
+        if (index < 2) {
+            return index;
+        } else {
+            return (fibonacci(index - 1)
+                    + (fibonacci(index - 2)));
+        }
+    }
+
 }
+
+// sk3 = 1 + 1;
+//            if (true) {
+//                fibonacci(sk2, sk3, max)
